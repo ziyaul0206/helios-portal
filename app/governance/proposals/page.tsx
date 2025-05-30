f58cdb1d6188af4f6d9ebd8d2ddb97abbdaa172b
cometsheepboy <cometsheepboy@gmail.com>
2025-05-30 12:58:24 -0700

    add create proposal modal [WIP]

--- app/governance/proposals/page.tsx
+++ app/governance/proposals/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useEffect, useRef, useState } from "react"
+import { useAccount } from "wagmi"
 import styles from "./page.module.scss"
 
 interface Proposal {
@@ -190,6 +191,17 @@ const AllProposals: React.FC = () => {
   const [proposals, setProposals] = useState(initialProposals)
   const [loading, setLoading] = useState(false)
   const loaderRef = useRef<HTMLDivElement | null>(null)
+  const { isConnected } = useAccount()
+  const [isLoading, setIsLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+
+  const handleCreateProposal = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+      setShowModal(true)
+    }, 1000)
+  }
 
   // Simulate loading more proposals
   const loadMoreProposals = () => {
@@ -232,41 +244,166 @@ const AllProposals: React.FC = () => {
   }, [loading])
 
   return (
-    <div className={styles["all-proposals"]}>
-      <h2 className={styles.sectionTitle}>All Proposals</h2>
-      <div className={styles["proposal-list"]}>
-        {proposals.map((proposal) => (
-          <div
-            key={proposal.id}
-            className={`${styles["proposal-item"]} ${proposal.resultClass}`}
-          >
-            <div className={styles["proposal-details"]}>
-              <p className={styles.meta}>{proposal.meta}</p>
-              <p className={styles.status}>{proposal.status}</p>
-              <p className={styles.votes}>{proposal.votes}</p>
+    <>
+      <div className={styles["all-proposals"]}>
+        <div className={styles.proposalContainer}>
+          <h2 className={styles.sectionTitle}>All Proposals</h2>
+          {isConnected && (
+            <button
+              className={styles["create-proposal"]}
+              onClick={handleCreateProposal}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <span className={styles.myloader}></span>Loading…
+                </>
+              ) : (
+                "Create Proposal"
+              )}
+            </button>
+          )}
+        </div>
+        <div className={styles["proposal-list"]}>
+          {proposals.map((proposal) => (
+            <div
+              key={proposal.id}
+              className={`${styles["proposal-item"]} ${proposal.resultClass}`}
+            >
+              <div className={styles["proposal-details"]}>
+                <p className={styles.meta}>{proposal.meta}</p>
+                <p className={styles.status}>{proposal.status}</p>
+                <p className={styles.votes}>{proposal.votes}</p>
 
-              <h3 className={styles.title}>{proposal.title}</h3>
-              <p className={styles.result}>
-                <span className={proposal.resultClass}>{proposal.result}</span>
-              </p>
-              <div className={styles["vote-bar"]}>
-                <div
-                  className={styles["vote-for"]}
-                  style={{ width: proposal.voteFor }}
-                ></div>
-                <div
-                  className={styles["vote-against"]}
-                  style={{ width: proposal.voteAgainst }}
-                ></div>
+                <h3 className={styles.title}>{proposal.title}</h3>
+                <p className={styles.result}>
+                  <span className={proposal.resultClass}>
+                    {proposal.result}
+                  </span>
+                </p>
+                <div className={styles["vote-bar"]}>
+                  <div
+                    className={styles["vote-for"]}
+                    style={{ width: proposal.voteFor }}
+                  ></div>
+                  <div
+                    className={styles["vote-against"]}
+                    style={{ width: proposal.voteAgainst }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div ref={loaderRef} className={styles.loader}>
+            {loading && <p>Loading...</p>}
+          </div>
+        </div>
+      </div>
+      {showModal && (
+        <div
+          className={styles.modalOverlay}
+          onClick={() => setShowModal(false)}
+        >
+          <div
+            className={styles.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className={styles.modalLeft}>
+              <div className={styles.padding1}>
+                <p>Title *</p>
+                <div className={styles.textInput}>
+                  <input type="text" className={styles.customPadding}></input>
+                </div>
+                <p>Description *</p>
+                <div className={styles.textArea}>
+                  <textarea
+                    className={styles.customPadding}
+                    rows={13}
+                  ></textarea>
+                </div>
+              </div>
+
+              <div className={styles.line} />
+
+              <div className={styles.padding1}>
+                <p
+                  className={`${styles.proposalTransactions} ${styles.fontBlack}`}
+                >
+                  Proposed transactions
+                </p>
+                <p className={`${styles.proposalTransactions} ${styles.mt1}`}>
+                  Proposed transactions will execute after a proposal passes and
+                  then gets executed.
+                </p>
+                <div className={styles.buttonRow}>
+                  <button className={styles.primaryButton}>
+                    Transfer from the treasury
+                  </button>
+                  <button className={styles.primaryButton}>
+                    Create a custom transaction
+                  </button>
+                </div>
+              </div>
+
+              <div className={styles.line} />
+
+              <div className={styles.padding1}>
+                <div className={styles.createDraftBut}>
+                  <div className={styles.customPadding}>Create Draft</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.modalRight}>
+              <div className={styles.proposalChecklist}>
+                <p className={styles.checklistTitle}>
+                  <strong>Proposal checklist</strong>
+                </p>
+                <p>
+                  <strong>1. Create your proposal</strong>
+                </p>
+                <p>
+                  Get started by drafting your proposal directly in the
+                  governance interface.
+                </p>
+
+                <p>
+                  <strong>2. Request sponsorship (if threshold not met)</strong>
+                </p>
+                <p>
+                  If you don&apos;t meet the required voting power threshold,
+                  you can request sponsorship from existing delegates. This
+                  allows your proposal to gain visibility and the necessary
+                  backing from the community.
+                </p>
+
+                <p>
+                  <strong>3. Submit as waiting for sponsorship</strong>
+                </p>
+                <p>
+                  If you don&apos;t have the voting power to post the proposal
+                  yourself, you can request a delegate with enough voting power
+                  to sponsor it. The delegate you choose can review your
+                  proposal and choose to sponsor it if they support it, pushing
+                  it onchain for voting. One note - you should coordinate with
+                  sponsor delegates so they know you&apos;re looking for
+                  sponsorship!
+                </p>
+
+                <p>
+                  <strong>4. Submit onchain (If threshold met)</strong>
+                </p>
+                <p>
+                  If you meet the voting power threshold, you can bypass the
+                  sponsorship phase and submit it onchain directly. This
+                  fast-tracks your proposal to the voting stage, giving the
+                  community the opportunity to decide on its implementation.
+                </p>
               </div>
             </div>
           </div>
-        ))}
-        <div ref={loaderRef} className={styles.loader}>
-          {loading && <p>Loading...</p>}
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
@@ -284,7 +421,6 @@ const ProposalDashboard: React.FC = () => {
     <div className={styles.dashboard}>
       <MyProposals proposal={myProposal} />
       <AllProposals />
-      {/* <button className={styles["create-proposal"]}>Create proposal</button> */}
     </div>
   )
 }
