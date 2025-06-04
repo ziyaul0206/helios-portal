4e5f9022e40c303939cfc12e64be84e648b8c26a
cometsheepboy <cometsheepboy@gmail.com>
2025-06-04 15:56:55 -0700

    skeleton voters

--- app/governance/voters/page.tsx
+++ app/governance/voters/page.tsx
@@ -1,3 +1,71 @@
+import { DelegateCard } from "@/components/delegatecard"
+import styles from "./voters.module.scss"
+
+const links = [
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  },
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  },
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  },
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  },
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  },
+  {
+    url: "https://example.com",
+    userid: "coby.eth",
+    hasX: true,
+    hasDiscord: true,
+    label: "2.256M HEL   0% Participation",
+    description:
+      "The magnitude system allows governance participants to propose controlled adjustments to an asset's weight in the staking consensus. The magnitude level determines how much the asset’s weight will increase or decrease, directly impacting staking incentives and APY distribution."
+  }
+]
+
 export default function VotersPage() {
-  return <div className="text-xl font-bold">Proposals content goes here.</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardGrid}>
+        {links.map((link, index) => (
+          <DelegateCard key={index} link={link} />
+        ))}
+      </div>
+    </div>
+  )
 }
---
+++ components/delegatecard/index.tsx
@@ -0,0 +1,75 @@
+import clsx from "clsx"
+import Image from "next/image"
+import { forwardRef } from "react"
+import styles from "./index.module.scss"
+
+interface Link {
+  url?: string
+  userid?: string
+  hasX?: boolean
+  hasDiscord?: boolean
+  label?: string
+  description?: string
+}
+
+interface CardProps {
+  link: Link
+  className?: string
+}
+
+export const DelegateCard = forwardRef<HTMLDivElement, CardProps>(
+  ({ link, className }, ref) => {
+    const {
+      url = "",
+      userid = "",
+      hasX = false,
+      hasDiscord = false,
+      label = "",
+      description = ""
+    } = link
+
+    return (
+      <div ref={ref} className={clsx(styles.card, className)}>
+        <div className={styles.cardTitle}>
+          <Image
+            className={styles.roundfull}
+            src="/img/logo.png"
+            alt="Discord"
+            width={30}
+            height={30}
+          />
+          {userid || "No UserID"}
+        </div>
+        <div className={styles.line} />
+        <p className={styles.cardLabel}>{label || "No label"}</p>
+        <p className={styles.cardDescription}>
+          {description || "No description"}
+        </p>
+        <div className={styles.cardActions}>
+          <div className={styles.iconscontainer}>
+            {hasX && (
+              <Image
+                src="/img/twitter.svg"
+                alt="twitter"
+                width={20}
+                height={20}
+              />
+            )}
+            {hasDiscord && (
+              <Image
+                src="/img/discord.svg"
+                alt="Discord"
+                width={25}
+                height={25}
+              />
+            )}
+          </div>
+
+          <div className={styles.delegatebutton}>Delegate</div>
+        </div>
+      </div>
+    )
+  }
+)
+
+DelegateCard.displayName = "DelegateCard"
