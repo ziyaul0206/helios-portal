06f456267da11946bc746409b340025a059165e9
jguyet <jguyet.weoohh@gmail.com>
2025-06-19 02:20:43 +0200

    disable bridge

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -5,35 +5,35 @@ import { Secure } from "./(components)/secure"
 import { Supported } from "./(components)/supported"
 import s from "./page.module.scss"
 import { AccountRecents } from "./(components)/accountrecents"
-// import { Message } from "@/components/message"
+import { Message } from "@/components/message"
 
 export default function Page() {
-  // return (
-  //   <Message title="Bridge temporarily unavailable" variant="danger">
-  //     The cross-chain bridge is temporarily disabled for maintenance. It will be
-  //     back soon.
-  //   </Message>
-  // )
-
   return (
-    <>
-      <Grid className={s.bridge}>
-        <Area area="a">
-          <Interface />
-        </Area>
-        <Area area="b">
-          <Supported />
-        </Area>
-        <Area area="c">
-          <Recents />
-        </Area>
-        <Area area="d">
-          <AccountRecents />
-        </Area>
-        <Area area="e">
-          <Secure />
-        </Area>
-      </Grid>
-    </>
+    <Message title="Bridge temporarily unavailable" variant="danger">
+      The cross-chain bridge is temporarily disabled for maintenance. It will be
+      back soon.
+    </Message>
   )
+
+  // return (
+  //   <>
+  //     <Grid className={s.bridge}>
+  //       <Area area="a">
+  //         <Interface />
+  //       </Area>
+  //       <Area area="b">
+  //         <Supported />
+  //       </Area>
+  //       <Area area="c">
+  //         <Recents />
+  //       </Area>
+  //       <Area area="d">
+  //         <AccountRecents />
+  //       </Area>
+  //       <Area area="e">
+  //         <Secure />
+  //       </Area>
+  //     </Grid>
+  //   </>
+  // )
 }
